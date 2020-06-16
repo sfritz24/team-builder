1a1d365cb6effd84931a67d1641c281be7f790f9
shanon <13fritsh@gmail.com>
2020-06-16 17:03:35 -0500

    added some dummydata and imported it to my app.js

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import characters from "./Components/DummyData";
 
 function App() {
+  const [mainCharacters, setMainCharacters] = useState(characters[0]);
+  const [secondChara, setSecondChara] = useState(characters[1]);
+  const [creatures, setCreatures] = useState(characters[2]);
+  const [booksMoives, setBooksMovies] = useState(characters[3]);
   return (
     <div className="App">
-      
+      <header>
+        <h1></h1>
+
+      </header>
     </div>
   );
 }
---
+++ team-builder/src/Components/DummyData.js
@@ -0,0 +1,55 @@
+const charaters = [
+    {
+        MainCharacters: [
+            {
+                name: "Kyo Sohma",
+                from: "Book",
+                first_apperance: "Fruits Basket",
+                type: "Human",
+                abilities: ["Change into cat", "Communicate with cats", "Martial arts"]
+            },
+            {
+                name: "Mario",
+                from: "Game",
+                first_apperance: "Donkey Kong",
+                type: "Human",
+                abiliteis: ["Jumping", "Utilizing power ups"]
+            }
+        ]
+    },
+    {
+        SecondaryCharacters: [
+            {
+                name: "Saki Hanajima",
+                from: "Book",
+                first_apperance: "Fruits Basket",
+                type: "Human",
+                abiliteis: ["Striking fear in peers", "Wave interpretation and manipulation"]
+            }
+        ]
+    },
+    {
+        Creatures: [
+            {
+                name: "Bowser",
+                from: "Comic",
+                first_apperance: "Let's Play",
+                type: "Dog",
+                abiliteis: ["Looking cute", "Protecting human", "Providing support"]
+            }
+        ]
+    },
+    {
+        BooksMoives: [
+            {
+                name: "The Immortals",
+                from: "Alson Noel",
+                first_apperance: "Evermore",
+                type: "Book",
+                key_points: ["Easy read", "Short chapters", "Relatable Characters"]
+            }
+        ]
+    }
+]
+
+export default charaters
